3d34e085bc83304590855926f0ee8c4de5a47c7d
TheMask8 <nhendriks2@gmail.com>
2021-01-06 17:10:12 +0100

    completed data storage, still to be tested

--- UFS1/apiExtract.py
+++ UFS1/apiExtract.py
@@ -2,6 +2,9 @@ import pytrends
 import pandas as pd
 import time
 import translators as ts
+from pathlib import Path
+import os
+from datetime import date
 
 
 countries = ['NL', 'DE', 'ES']
@@ -27,4 +30,28 @@ def translate(text, country):
     else:
         translations = [ts.google(text, from_language='en', to_language=lang)]
     return translations
+
+
+def saveResult(df, file_name, folder_name=''):
+    """
+    Save the results in a new or existing folder of the day
+    :param df: data frame to save
+    :param file_name: filename required
+    :param folder_name: a folder name
+    """
+    data_path = Path.cwd().absolute().parents[0].as_posix() + "/Data"
+    folder_name = folder_name + '___' + date.today().strftime('%d_%m_%Y')
+    folder_path = data_path + "/" + folder_name
+    if os.path.exists(folder_path):
+        df.to_csv(folder_path + "/" + file_name + ".txt", header=None, index=None, sep = ', ', mode='a')
+    else:
+        try:
+            os.mkdir(folder_path)
+            df.to_csv(folder_path + "/" + file_name + ".txt", header=None, index=None, sep=', ', mode='a')
+        except OSError:
+            print("Failed to create folder, file saved in Data folder")
+            df.to_csv(data_path + "/" + file_name + ".txt", header=None, index=None, sep = ', ', mode='a')
+
+
+
 
