f5b124d9b85fba517a023159852ad0a8d7d7133b
TheMask8 <nhendriks2@gmail.com>
2021-01-20 11:21:51 +0100

    Requirements uploaded

--- UFS1/main.py
+++ UFS1/main.py
@@ -3,11 +3,9 @@ import pandas as pd
 
 countries = ['NL', 'DE']
 years = range(2016, 2021)
-frames = []
+df = {}
 for country in countries:
-    frames += ApiExtract.extract(years, country)
+    df[country] = pd.concat(ApiExtract.extract(years, country))
 
-df = pd.concat(frames)
-groups = df.groupby('category')['keyword'].agg('unique')
 
 
