0b432bb4e30f27e5e26c73ee4eaa08038c6785b8
TheMask8 <nhendriks2@gmail.com>
2021-01-21 11:13:19 +0100

    Modified dates to allow for correct montly representation

--- UFS1/main.py
+++ UFS1/main.py
@@ -1,11 +1,12 @@
-import ApiExtract
+from ApiExtract import extract
 import pandas as pd
 
 countries = ['NL', 'DE']
 years = range(2016, 2021)
 df = {}
 for country in countries:
-    df[country] = pd.concat(ApiExtract.extract(years, country))
+    df[country] = extract(years, country)
+
 
 
 
