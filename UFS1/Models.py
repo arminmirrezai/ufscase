8ceae82385007011bb369b85240ba1fa7d6ef522
TheMask8 <nhendriks2@gmail.com>
2021-02-01 12:03:29 +0100

    Arima model implemented

--- UFS1/Models.py
+++ UFS1/Models.py
@@ -1,101 +1,140 @@
-
-import os
-import ApiExtract
-import pandas as pd
+import numpy as np
 import pmdarima as pm
-import matplotlib.pyplot as plt
-from statsmodels.tsa.arima.model import ARIMA
+from Description import Data
+from scipy.stats.distributions import chi2
 
-def multiPlot(trainSets, testSets, forecasts):
 
-    fig, ax = plt.subplots(len(testSets.columns), 1)
-    for i in range(len(testSets.columns)):
-        ax[i].plot(trainSets[trainSets.columns[i]], color = 'black')
-        ax[i].plot(testSets[testSets.columns[i]], color = 'green')
-        ax[i].plot(forecasts[forecasts.columns[i]], 'r--')
+class Arima:
 
-    plt.show()
+    def __init__(self, df):
+        self.df = df
+        self.dd = Data(df)
+        self.model = pm.ARIMA
+        self.stats = ""
 
-def runKeywords(df, keywords):
-    trainSets = pd.DataFrame(columns=keywords) 
-    testSets = pd.DataFrame(columns=keywords)
-    forecasts = pd.DataFrame(columns = keywords)
-    for keyword in keywords:
-        (trainData, testData) = readData(df, keyword)
-        trainSets[keyword] = trainData
-        testSets[keyword] = testData
-        forecasts[keyword] = autoArima(trainData, testData)[1]
+    @property
+    def residuals(self):
+        return self.model.resid() if self.model is not None else 0
 
-    return trainSets, testSets, forecasts
+    @property
+    def aic(self):
+        return self.model.aic
 
-def singlePlot(trainData, testData, arimaForecast, SARIMA):
+    @property
+    def log_likelihood(self):
+        return len(self.model.params()) - self.aic/2
 
-    print("the lowest value is", min(arimaForecast))
+    def time_series(self, keyword):
+        ts = self.df[self.df.keyword == keyword]['interest']
+        ts.index = self.df.startDate.unique()
+        return ts
 
-    plt.figure()
-    plt.plot(trainData, color = 'black', label = "Training data")
-    plt.plot(testData, color = 'green', label = "Testing data")
-    plt.plot(arimaForecast, 'r-.', label = "Prediction")
-    plt.legend(loc = 'upper left')
-    plt.show()
+    def fit(self, keyword):
+        """
+        Fit the best arima or sarima model for the keyword
+        :param keyword: keyword
+        :return: fitted model
+        """
+        stationary, has_trend = self.dd.statistics.stationary(keyword)
+        if stationary:
+            arima = pm.auto_arima(self.time_series(keyword), seasonal=False, stationary=stationary, d=0,
+                                  trend='ct' if has_trend else 'c', with_intercept=True)
+            sarima = pm.auto_arima(self.time_series(keyword), seasonal=True, stationary=stationary, d=0,
+                                   trend='ct' if has_trend else 'c', with_intercept=True)
+        else:
+            stationary, has_trend = self.dd.statistics.stationary(keyword, first_difference=True)
+            arima = pm.auto_arima(self.time_series(keyword), seasonal=False, stationary=stationary, d=1,
+                                  trend='ct' if has_trend else 'c', with_intercept=True)
+            sarima = pm.auto_arima(self.time_series(keyword), seasonal=False, stationary=stationary, d=1,
+                                   trend='ct' if has_trend else 'c', with_intercept=True)
+        if arima.order == sarima.order:
+            self.model = arima
+        else:
+            self.model = arima if self.llr_test(arima, sarima) else sarima
+        self._write_stats(keyword)
+        return self.model
 
-    SARIMA.plot_diagnostics()
-    plt.show()
+    def _write_stats(self, keyword):
+        """
+        Write the stats of the model
+        :param keyword:
+        :return:
+        """
+        self.stats += keyword + ":\n"
+        self.stats += "Order: " + str(self.model.order) + "\n"
+        self.stats += "SSR: " + str(sum(np.square(self.residuals)))
+        self.stats += "AIC: " + str(self.aic)
 
-def manualArima(trainData, testData, order, seasonal_order):
-    
-    SARIMAestimation = ARIMA(trainData ,order=order, seasonal_order=seasonal_order).fit()
-    forecast = SARIMAestimation.predict(start=testData.index[0], end=testData.index[len(testData.index)-1])
-    arimaForecast = pd.Series(forecast, index = testData.index)
+    @staticmethod
+    def llr_test(model1: pm.ARIMA, model2: pm.ARIMA, significance=0.05):
+        """
+        Likelihood ratio test
+        :param model1: H0 model
+        :param model2: HA model
+        :param significance: significance level
+        :return: H0 result test
+        """
+        k1 = len(model1.params())
+        k2 = len(model2.params())
+        lr = 2 * (k1 - k2) + model2.aic - model1.aic
+        return chi2.sf(lr, k2 - k1) > significance
 
-    return SARIMAestimation, arimaForecast
-
-def autoArima(trainData, testData):
-    
-    SARIMA = pm.auto_arima(trainData, start_p=1, start_q=1,test='adf',max_p=3, max_q=3, m=len(testData) ,start_P=0, seasonal=True,d=None, D=1, trace=True,
-                         error_action='ignore',  suppress_warnings=True, stepwise=True, trend = 'ct')
-    forecast = SARIMA.predict(n_periods = len(testData))
-    arimaForecast = pd.Series(forecast, index = testData.index)
-
-    return SARIMA, arimaForecast
-
-def readData(df, keyword):
-
-    data = df[df.keyword == keyword][['interest', 'startDate']]
-    data = data.interest.rename(index = data.startDate)
-
-    startYear = data.index[0].year
-    endYear = data.index[len(data)-1].year + 1
-
-    splitThreshold = int(len(data)*(1-1/(endYear - startYear)))
-
-    return data[:splitThreshold], data[splitThreshold+1:]
-
-def main():
-
-    ################ VARIABLES:
-    country = 'NL'
-    startYear = 2016
-    endYear = 2021
-    keyword = 'aioli'
-    
-    ################ MAIN CODE:
-    df = ApiExtract.extract(range(startYear, endYear), country)
-    keywords = df.keyword.unique()
-
-    (trainData, testData) = readData(df, keyword)
-    #(SARIMA, autoForecastData) = autoArima(trainData, testData)
-
-    (SARIMAestimation, manualForecacastData) = manualArima(trainData, testData, (0,0,0), (1,1,0,52))
-
-    #(trainSets, testSets, arimaModels) = runKeywords(df, keywords[:10])
-
-    ################ OUTPUT AND PLOTTING:
-    #multiPlot(trainSets, testSets, arimaModels)
-    
-    #singlePlot(trainData, testData, autoForecastData, SARIMA)
-    
-    singlePlot(trainData, testData, manualForecacastData, SARIMAestimation)
-
-if __name__ == "__main__":
-    main()
+#
+#
+# def multiPlot(trainSets, testSets, forecasts):
+#
+#     fig, ax = plt.subplots(len(testSets.columns), 1)
+#     for i in range(len(testSets.columns)):
+#         ax[i].plot(trainSets[trainSets.columns[i]], color = 'black')
+#         ax[i].plot(testSets[testSets.columns[i]], color = 'green')
+#         ax[i].plot(forecasts[forecasts.columns[i]], 'r--')
+#
+#     plt.show()
+#
+#
+#
+#
+#
+# def runKeywords(df, keywords):
+#     trainSets = pd.DataFrame(columns=keywords)
+#     testSets = pd.DataFrame(columns=keywords)
+#     forecasts = pd.DataFrame(columns = keywords)
+#     for keyword in keywords:
+#         (trainData, testData) = readData(df, keyword)
+#         trainSets[keyword] = trainData
+#         testSets[keyword] = testData
+#         forecasts[keyword] = autoArima(trainData, testData)[1]
+#
+#     return trainSets, testSets, forecasts
+#
+# def singlePlot(trainData, testData, arimaForecast, SARIMA):
+#
+#     print("the lowest value is", min(arimaForecast))
+#
+#     plt.figure()
+#     plt.plot(trainData, color = 'black', label = "Training data")
+#     plt.plot(testData, color = 'green', label = "Testing data")
+#     plt.plot(arimaForecast, 'r-.', label = "Prediction")
+#     plt.legend(loc = 'upper left')
+#     plt.show()
+#
+#     SARIMA.plot_diagnostics()
+#     plt.show()
+#
+# def manualArima(trainData, testData, order, seasonal_order):
+#
+#     SARIMAestimation = ARIMA(trainData ,order=order, seasonal_order=seasonal_order).fit()
+#     forecast = SARIMAestimation.predict(start=testData.index[0], end=testData.index[len(testData.index)-1])
+#     arimaForecast = pd.Series(forecast, index = testData.index)
+#
+#     return SARIMAestimation, arimaForecast
+#
+# def autoArima(trainData, testData):
+#
+#     SARIMA = pm.auto_arima(trainData, start_p=1, start_q=1,test='adf',max_p=3, max_q=3, m=len(testData) ,start_P=0, seasonal=True,d=None, D=1, trace=True,
+#                          error_action='ignore',  suppress_warnings=True, stepwise=True, trend = 'ct')
+#     forecast = SARIMA.predict(n_periods = len(testData))
+#     arimaForecast = pd.Series(forecast, index = testData.index)
+#
+#     return SARIMA, arimaForecast
+#
