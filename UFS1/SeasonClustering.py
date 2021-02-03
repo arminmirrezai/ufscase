1ff5283f0279fcaf4601bd988a87d1fc9a3efd5b
Your Name <arminmirrezai@gmail.com>
2021-02-03 11:29:49 +0100

    pull

--- UFS1/SeasonClustering.py
+++ UFS1/SeasonClustering.py
@@ -22,7 +22,7 @@ all_series = []
 for keyword in df.keyword.unique():
     product = df[df.keyword == keyword]['interest']
     time_series = pd.Series(product.tolist(), index = pd.date_range('1-1-2017', periods=len(product), freq='W'), name = keyword)
-    stl = STL(time_series).fit()
+    stl = STL(time_series, robust=True).fit()
     # stl_series.append(stl)
     seasonal = stl.seasonal
     all_series.append(seasonal)
@@ -31,22 +31,24 @@ for keyword in df.keyword.unique():
 
 # Hierarchical clustering_____________________________________________________________________________________________
 
-for i in range(len(series)):
-    length = len(series[i])
-    # series[i] = series[i].values.reshape((length, 1))
-    series[i] = series[i].reshape((length, 1))
+for i in range(len(all_series)):
+    length = len(all_series[i])
+    all_series[i] = all_series[i].values.reshape((length, 1))
+    # series[i] = series[i].reshape((length, 1))
 
-from sktime.distances.elastic_cython import dtw_distance
+# from sktime.distances.elastic_cython import dtw_distance
+from dtw import *
 
-n_series = len(series)
+n_series = len(all_series)
 distance_matrix = np.zeros(shape=(n_series, n_series))
 
 for i in range(n_series):
     for j in range(n_series):
-        x = series[i]
-        y = series[j]
+        x = all_series[i]
+        y = all_series[j]
         if i != j:
-            dist = dtw_distance(x, y)
+            # dist = dtw_distance(x, y)
+            dist = dtw(x, y, keep_internals=True).distance
             distance_matrix[i, j] = dist
 
 # for i in range(n_series):
@@ -71,10 +73,10 @@ def hierarchical_clustering(dist_mat, method='complete'):
     if method == 'ward':
         Z = ward(distance_matrix)
     
-    # fig = plt.figure(figsize=(16, 8))
-    # dn = dendrogram(Z)
-    # plt.title(f"Dendrogram for {method}-linkage with dtw distance")
-    # plt.show()
+    fig = plt.figure(figsize=(16, 8))
+    dn = dendrogram(Z)
+    plt.title(f"Dendrogram for {method}-linkage with dtw distance")
+    plt.show()
     
     return Z
 
@@ -83,86 +85,8 @@ linkage_matrix = hierarchical_clustering(distance_matrix)
 from scipy.cluster.hierarchy import fcluster
 
 # select maximum number of clusters
-cluster_labels = fcluster(linkage_matrix, 3, criterion='maxclust')
+cluster_labels = fcluster(linkage_matrix, 20, criterion='maxclust')
 print(np.unique(cluster_labels))
-
-# distances = []                  ###########################################ELBOW PLOT<<<<<<<<<<>>>>>>>>>>>
-# bestClusters = []
-# for c in range(1,10+1):
-#     print(c)
-#     cluster_labels = fcluster(linkage_matrix, t=c, criterion='maxclust')
-#     numbOfClusters = max(cluster_labels)
-#     cluster1 = []
-#     cluster2 = []
-#     cluster3 = []
-#     cluster4 = []
-#     cluster5 = []
-#     cluster6 = []
-#     cluster7 = []
-#     cluster8 = []
-#     cluster9 = []
-#     cluster10 = []
-#     for i in range(len(cluster_labels)):
-#         if cluster_labels[i] == 1:
-#             cluster1.append(seasonal_series[:,i])
-#         elif cluster_labels[i] == 2:
-#             cluster2.append(seasonal_series[:,i])
-#         elif cluster_labels[i] == 3:
-#             cluster3.append(seasonal_series[:,i])
-#         elif cluster_labels[i] == 4:
-#             cluster4.append(seasonal_series[:,i])
-#         elif cluster_labels[i] == 5:
-#             cluster5.append(seasonal_series[:,i])
-#         elif cluster_labels[i] == 6:
-#             cluster6.append(seasonal_series[:,i])
-#         elif cluster_labels[i] == 7:
-#             cluster7.append(seasonal_series[:,i])
-#         elif cluster_labels[i] == 8:
-#             cluster8.append(seasonal_series[:,i])
-#         elif cluster_labels[i] == 9:
-#             cluster9.append(seasonal_series[:,i])
-#         elif cluster_labels[i] == 10:
-#             cluster10.append(seasonal_series[:,i])
-        
-#     cluster1 = np.transpose(np.array(cluster1))
-#     cluster2 = np.transpose(np.array(cluster2))
-#     cluster3 = np.transpose(np.array(cluster3))
-#     cluster4 = np.transpose(np.array(cluster4))
-#     cluster5 = np.transpose(np.array(cluster5))
-#     cluster6 = np.transpose(np.array(cluster6))
-#     cluster7 = np.transpose(np.array(cluster7))
-#     cluster8 = np.transpose(np.array(cluster8))
-#     cluster9 = np.transpose(np.array(cluster9))
-#     cluster10 = np.transpose(np.array(cluster10))
-
-#     if i ==6:
-#         bestClusters = [cluster1, cluster2, cluster3, cluster4, cluster5, cluster6, cluster7]
-    
-#     averageDists = []
-
-#     for cluster in [cluster1, cluster2, cluster3, cluster4, cluster5, cluster6, cluster7, cluster8, cluster9, cluster10]:
-#         if len(cluster) == 0:
-#             continue
-#         n_series = len(cluster[0])
-#         totDist = 0
-#         for i in range(n_series):
-#             for j in range(n_series):
-#                 x = cluster[:,i]
-#                 y = cluster[:,j]
-#                 if i != j:
-#                     dist = dtw(x, y)
-#                     totDist += dist
-#         if totDist > 0:
-#             averageDists.append(totDist/len(cluster))
-
-#     distances.append(np.sum(averageDists)/c)
-
-# plt.figure()
-# plt.plot(np.arange(1,10+1), distances)
-# plt.title("Elbow plot of the average distance within differently sized clusters")
-# plt.ylabel("Avg dist wthin clusters")
-# plt.xlabel("Number of clusters")
-# plt.show()                                 #END##########################################ELBOW PLOT<<<<<<<<<<>>>>>>>>>>>END
 
 # kNN_____________________________________________________________________________________________
 
@@ -192,14 +116,20 @@ print(clustering.labels_)
 
 # plot_____________________________________________________________________________________________
 
-for i in range(3):
+# for i in range(3):
+#     cluster = np.where(cluster_labels == i)
+#     fig, axs = plt.subplots(cluster[0].size)
+#     if cluster[0].size == 1:
+#         axs.plot(all_series[cluster[0][0]])
+#     else:
+#         for j in range(cluster[0].size):
+#             axs[j].plot(all_series[cluster[0][j]]) 
+#     plt.show()
+
+for i in range(20):
     cluster = np.where(cluster_labels == i)
-    fig, axs = plt.subplots(cluster[0].size)
-    if cluster[0].size == 1:
-        axs.plot(all_series[cluster[0][0]])
-    else:
-        for j in range(cluster[0].size):
-            axs[j].plot(all_series[cluster[0][j]]) 
+    for j in range(cluster[0].size):
+        plt.plot(all_series[cluster[0][j]])
     plt.show()
 
 # F-measure_____________________________________________________________________________________________
