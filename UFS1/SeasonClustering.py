d63a549faf03e7a4193628f3b8073f7b7a78f66c
TheMask8 <nhendriks2@gmail.com>
2021-02-20 00:08:00 +0100

    Refactored clustering

--- UFS1/SeasonClustering.py
+++ UFS1/SeasonClustering.py
@@ -10,6 +10,8 @@ from scipy.cluster.hierarchy import single, complete, average, ward, dendrogram,
 from scipy.spatial.distance import pdist, squareform
 from sklearn import metrics
 from c_index import calc_c_index
+from pyclustering.cluster.kmedoids import kmedoids
+from pyclustering.cluster.clarans import clarans
 
 
 # Import data________________________________________________________________________________________________________
@@ -27,21 +29,48 @@ df = ApiExtract.extract(years,'NL')
 from Description import Data
 data = Data(df)
 low_sparsity = []
-sparsity = []
+mid_sparsity = []
+high_sparsity = []
 
 for word in df.keyword.unique():
-    if data.statistics.sparsity(word) > 0.1:
-        sparsity.append(word)
+    if data.statistics.sparsity(word) > 0.9:
+        high_sparsity.append(word)
+    elif data.statistics.sparsity(word) > 0.3:
+        mid_sparsity.append(word)
     else:
         low_sparsity.append(word)
 
-sparsity_series = []
+low_sparsity_series = []
 for keyword in low_sparsity:
     product = df[df.keyword == keyword]
     for country in product.country.unique():
         time_series = product[product.country == country]["interest"]
         time_series.index = product.startDate.unique()
-        sparsity_series.append(time_series.tolist())
+        low_sparsity_series.append(time_series.tolist())
+
+mid_sparsity_series = []
+for keyword in mid_sparsity:
+    product = df[df.keyword == keyword]
+    for country in product.country.unique():
+        time_series = product[product.country == country]["interest"]
+        time_series.index = product.startDate.unique()
+        mid_sparsity_series.append(time_series.tolist())
+
+no_sparsity = []
+for word in df.keyword.unique():
+    if data.statistics.sparsity(word) == 0:
+        no_sparsity.append(word)
+
+no_sparsity_series = []
+for keyword in no_sparsity:
+    product = df[df.keyword == keyword]
+    for country in product.country.unique():
+        time_series = product[product.country == country]["interest"]
+        time_series.index = product.startDate.unique()
+        no_sparsity_series.append(time_series.tolist())
+
+
+series = low_sparsity_series
 
 
 # -------------------------------------------------------------------------------------------------------------------
@@ -49,29 +78,29 @@ for keyword in low_sparsity:
 # -------------------------------------------------------------------------------------------------------------------
 
 # Manhattan Distance_________________________________________________________________________________________________
-ts_matrix = np.array(sparsity_series) 
+ts_matrix = np.array(series) 
 distance_matrix = squareform(pdist(ts_matrix, 'cityblock'))
 
 # Euclidean Distance_________________________________________________________________________________________________
-ts_matrix = np.array(sparsity_series) 
+ts_matrix = np.array(series) 
 distance_matrix = squareform(pdist(ts_matrix, 'euclidean'))
 
 # Chebyshev Distance_________________________________________________________________________________________________
-ts_matrix = np.array(sparsity_series) 
+ts_matrix = np.array(series) 
 distance_matrix = squareform(pdist(ts_matrix, 'chebyshev'))
 
 # Dynamic Time Warping_______________________________________________________________________________________________
-n_series = len(sparsity_series)
+n_series = len(series)
 distance_matrix = np.zeros(shape=(n_series, n_series))
 
 for i in range(n_series):
     for j in range(n_series):
-        x = sparsity_series[i]
-        y = sparsity_series[j]
+        x = series[i]
+        y = series[j]
         if i != j:
-            # dist = dtw_distance(x, y)
             dist = dtw(x, y, keep_internals=True).distance
             distance_matrix[i, j] = dist
+
 
 # Longest Common Subsequence_________________________________________________________________________________________
 from numba import jit as _jit
@@ -103,15 +132,31 @@ def lcss_dist(X, Y, delta, epsilon):
     dist = _lcss_dist(X, Y, delta, epsilon)
     return dist
 
-n_series = len(sparsity_series)
+n_series = len(series)
 distance_matrix = np.zeros(shape=(n_series, n_series))
 
 for i in range(n_series):
     for j in range(n_series):
-        x = sparsity_series[i]
-        y = sparsity_series[j]
+        x = series[i]
+        y = series[j]
         if i != j:
             dist = lcss_dist(x, y, delta=np.inf, epsilon=0.5)
+            distance_matrix[i, j] = dist
+
+
+# Discrete Fourier Transform_________________________________________________________________________________________
+n_series = len(series)
+distance_matrix = np.zeros(shape=(n_series, n_series))
+for i in range(n_series):
+    for j in range(n_series):
+        x = series[i]
+        y = series[j]
+        if i != j:
+            xdft = np.fft.rfft(x)
+            ydft = np.fft.rfft(y)
+            num_freq = 50
+            dist = np.linalg.norm(np.asarray(sorted(xdft, reverse=True)[0:num_freq]) - np.asarray(sorted(ydft, reverse=True)[0:num_freq])) 
+            # dist = np.linalg.norm(np.asarray(sorted(xdft, reverse=True)) - np.asarray(sorted(ydft, reverse=True))) 
             distance_matrix[i, j] = dist
 
 
@@ -120,7 +165,7 @@ for i in range(n_series):
 # -------------------------------------------------------------------------------------------------------------------
 
 # Hierarchical clustering____________________________________________________________________________________________
-def hierarchical_clustering(dist_mat, method='complete'):
+def hierarchical_clustering(distance_matrix, method):
     if method == 'complete':
         Z = complete(distance_matrix)
     if method == 'single':
@@ -137,7 +182,34 @@ def hierarchical_clustering(dist_mat, method='complete'):
     
     return Z
 
-linkage_matrix = hierarchical_clustering(distance_matrix)
+linkage_matrix = hierarchical_clustering(distance_matrix, 'complete')
+linkage_matrix = hierarchical_clustering(distance_matrix, 'ward')
+
+# K-Medoids clustering_______________________________________________________________________________________________
+
+def kMedoids(distance_matrix, num_clusters):
+    initial_medoids =  np.random.uniform(0,len(distance_matrix), num_clusters).astype(int).tolist()
+    kmedoids_instance = kmedoids(distance_matrix, initial_medoids, data_type='distance_matrix')
+    kmedoids_instance.process()
+    clusters = kmedoids_instance.get_clusters()
+    cluster_labels = np.zeros(len(distance_matrix))
+    for i in range(len(clusters)):
+        for j in range(len(clusters[i])):
+            cluster_labels[clusters[i][j]] = i+1
+    return cluster_labels
+
+
+# CLARANS clustering_________________________________________________________________________________________________
+
+def CLARANS(distance_matrix, num_clusters):
+    clarans_instance = clarans(data=series, number_clusters=num_clusters, numlocal=2, maxneighbor=250)
+    clarans_instance.process()
+    clusters = clarans_instance.get_clusters()
+    cluster_labels = np.zeros(len(distance_matrix))
+    for i in range(len(clusters)):
+        for j in range(len(clusters[i])):
+            cluster_labels[clusters[i][j]] = i+1
+    return cluster_labels
 
 
 # -------------------------------------------------------------------------------------------------------------------
@@ -149,25 +221,94 @@ linkage_matrix = hierarchical_clustering(distance_matrix)
 CH = []
 for i in range(10):
     cluster_labels = fcluster(linkage_matrix, i+2, criterion='maxclust')
-    CH.append(metrics.calinski_harabasz_score(sparsity_series, cluster_labels))
+    # cluster_labels = kMedoids(distance_matrix, i+2)
+    CH.append(metrics.calinski_harabasz_score(distance_matrix, cluster_labels))
 
 # maximize
 num_clusters = CH.index(max(CH)) + 2
 cluster_labels = fcluster(linkage_matrix, num_clusters, criterion='maxclust')
+# cluster_labels = kMedoids(distance_matrix, num_clusters)
+CH
 
 
 # C index____________________________________________________________________________________________________________
-X = np.array(sparsity_series)
+X = np.array(series)
 
 C = []
 for i in range(15):
     cluster_labels = fcluster(linkage_matrix, i+2, criterion='maxclust')
+    # cluster_labels = kMedoids(distance_matrix, i+2)
     cindex = calc_c_index(X, cluster_labels)
     C.append(cindex)
 
 # minimize
 num_clusters = C.index(min(C)) + 2
 cluster_labels = fcluster(linkage_matrix, num_clusters, criterion='maxclust')
+# cluster_labels = kMedoids(distance_matrix, num_clusters)
+C
+
+
+# Silhouette score___________________________________________________________________________________________________
+from sklearn.metrics import silhouette_score
+
+silhouette = []
+for i in range(15):
+    cluster_labels = fcluster(linkage_matrix, i+2, criterion='maxclust')
+    # cluster_labels = kMedoids(distance_matrix, i+2)
+    silhouette_avg = silhouette_score(distance_matrix, cluster_labels, metric="precomputed")
+    silhouette.append(silhouette_avg)
+
+# minimize
+num_clusters = silhouette.index(max(silhouette)) + 2
+cluster_labels = fcluster(linkage_matrix, num_clusters, criterion='maxclust')
+# cluster_labels = kMedoids(distance_matrix, num_clusters)
+silhouette
+
+
+# Gamma score________________________________________________________________________________________________________
+def Baker_Hubert_Gamma(distance_matrix, cluster_labels):
+    splus=0
+    sminus=0
+    pair_dis=squareform(distance_matrix)
+    # numPair=len(pairDis)
+    num_pair = len(pair_dis)
+    # temp = np.zeros((len(self.classLabel),2))
+    temp = np.zeros((len(cluster_labels),2))
+    temp[:,0]= cluster_labels
+    vecB= pdist(temp)
+    #iterate through all the pairwise comparisons
+    for i in range(num_pair-1):
+        for j in range(i+1,num_pair):
+            if vecB[i]>0 and vecB[j]==0:
+                #heter points smaller than homo points
+                if pair_dis[i]<pair_dis[j]:
+                    splus=splus+1
+                #heter points larger than homo points
+                if pair_dis[i]>vecB[j]:
+                    sminus=sminus+1
+            if vecB[i]==0 and vecB[j]>0:
+                #heter points smaller than homo points
+                if pair_dis[j]<pair_dis[i]:
+                    splus=splus+1
+                #heter points larger than homo points
+                if pair_dis[j]>vecB[i]:
+                    sminus=sminus+1
+    #compute the fitness
+    gamma = (splus-sminus)/(splus+sminus)
+    return gamma
+
+gamma = []
+for i in range(15):
+    # cluster_labels = fcluster(linkage_matrix, i+2, criterion='maxclust')
+    cluster_labels = kMedoids(distance_matrix, i+2)
+    g = Baker_Hubert_Gamma(distance_matrix, cluster_labels)
+    gamma.append(g)
+
+# minimize
+num_clusters = silhouette.index(max(silhouette)) + 2
+cluster_labels = fcluster(linkage_matrix, num_clusters, criterion='maxclust')
+# cluster_labels = kMedoids(distance_matrix, num_clusters)
+silhouette
 
 
 # -------------------------------------------------------------------------------------------------------------------
@@ -177,13 +318,14 @@ cluster_labels = fcluster(linkage_matrix, num_clusters, criterion='maxclust')
 # Plot_______________________________________________________________________________________________________________
 for i in range(num_clusters):
     cluster = np.where(cluster_labels == i+1)
-    mean_series = np.zeros(np.array(sparsity_series[cluster[0][0]]).size)
+    mean_series = np.zeros(np.array(series[cluster[0][0]]).size)
     for j in range(cluster[0].size):
-        mean_series += np.array(sparsity_series[cluster[0][j]])
-        plt.plot(sparsity_series[cluster[0][j]], 'k')
+        mean_series += np.array(series[cluster[0][j]])
+        plt.plot(series[cluster[0][j]], 'k')
     mean_series = mean_series / cluster[0].size
     plt.plot(mean_series, 'r')
     plt.show()
+
 
 # F-measure__________________________________________________________________________________________________________
 from Decompositions import Decompose
@@ -208,15 +350,13 @@ for keyword in high_seasonal:
     time_series = pd.Series(product.tolist(), index = pd.date_range('1-1-2017', periods=len(product), freq='W'), name = keyword)
     seasonal_series.append(time_series.tolist())
 
+
 # all/stl series_____________________________________________________________________________________________________
 all_series = []
-# stl_series = []
 
 for keyword in df.keyword.unique():
     product = df[df.keyword == keyword]
     for country in product.country.unique():
         time_series = product[product.country == country]["interest"]
         time_series.index = product.startDate.unique()
-        # stl = STL(time_series, robust=True).fit()
-        # stl_series.append(stl)
         all_series.append(time_series.tolist())
