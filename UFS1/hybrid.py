02fafa734fb37d2c82ab1f297c6456d88d0ed8ea
safouaanee <77001725+safouaanee@users.noreply.github.com>
2021-03-01 17:50:55 +0100

    hybrid for clusters and keywords

--- UFS1/hybrid.py
+++ UFS1/hybrid.py
@@ -1,22 +1,13 @@
-from re import L, T
-import ApiExtract
-import pandas as pd
-import numpy as np
-import pmdarima as pm
-import itertools
-import matplotlib.pyplot as plt
-from operator import index
-from sklearn.metrics import mean_absolute_error
-import warnings
-warnings.filterwarnings("ignore")
-from scipy import stats
-import time
-import multiprocessing as mp
 import os
-os.environ['TF_CPP_MIN_LOG_LEVEL'] = '1' 
+import itertools
+import numpy as np
+import pandas as pd
+import multiprocessing as mp
+import matplotlib.pyplot as plt
+from sklearn.metrics import mean_absolute_error as mae
+from scipy import stats
+import ApiExtract
 from Models import Arima, Lstm
-from ApiExtract import extract
-import Description
 import sys
 
 def plot_hybrid(trainData, testData, sarima_forecast, lstm_forecast):
@@ -54,14 +45,13 @@ def plot_hybrid(trainData, testData, sarima_forecast, lstm_forecast):
     plt.title("Normal Q-Q plot for hybrid residuals")
     plt.show()
 
+
 def getForecasts(sarima, optimal_params, test_data):
 
-    horizon = len(test_data.index)
     sarima_forecast = dd.predict()
-    
+
     train_resids = sarima.resid().reshape(-1,1) #4 jaar 
     test_resids = np.array(test_data - sarima_forecast).reshape(-1,1) #1 jaar
-
     # plt.figure()
     # plt.plot(train_resids)
     # plt.show()
@@ -69,57 +59,46 @@ def getForecasts(sarima, optimal_params, test_data):
     lstm_forecast = lstm(optimal_params, train_resids, test_resids, 0)[1]
     lstm_forecast = pd.Series(lstm_forecast, index=test_data.index)
 
-    arima_performance = calculate_performance(test_data, sarima_forecast)
-    lstm_performance = calculate_performance(test_data, sarima_forecast + lstm_forecast)
-    
-    if arima_performance[1] < lstm_performance[1]:
-        print(f"\nHybrid model did not perform well ({arima_performance[1]},{lstm_performance[1]}), performing a grid search once again:\n")
-        optimal_params =  runLstm(train_resids, test_resids)
-        sarima_forecast, lstm_forecast = getForecasts(sarima, optimal_params, test_data)
-    else:
-        print(f"\nThe mean absolute error goes from {arima_performance[1]} to {lstm_performance[1]} after fitting the arima residuals using LSTM!")
+    arima_performance = round(mae(test_data, sarima_forecast), 3)
+    lstm_performance = round(mae(test_data, sarima_forecast + lstm_forecast), 3)
+
+    print(f"\nThe mean absolute error goes from {arima_performance} to {lstm_performance} after fitting the arima residuals using LSTM")
 
     return sarima_forecast, lstm_forecast
 
-def calculate_performance(y_true, y_pred):
-    
-    mse = mean_squared_error(y_true, y_pred)
-    mae = mean_absolute_error(y_true, y_pred)
-    rmse = np.sqrt(mse)
-
-    return round(mse, 3), round(mae, 3), round(rmse, 3)
 
 def lstm(params, train_resids, test_resids, teller):
+
     if teller == 0: print("Fitting a hybrid model using the best parameter combination .....")
     else: print(f"Computing performance for parameter combination {teller}")
-        
-    minimum = min(train_resids.min(axis=0), test_resids.min(axis=0))
-    maximum = max(train_resids.max(axis=0), test_resids.max(axis=0))
-    scaled_train_resids = 4 * (train_resids - minimum) / (maximum - minimum) - 2
-    scaled_test_resids = 4 * (test_resids - minimum) / (maximum - minimum) - 2
-    
+
     [look_back, output_nodes, nb_epoch, batch_size] = [elt for elt in params]
-    m = Lstm(scaled_train_resids, scaled_test_resids, look_back,  output_nodes, nb_epoch, batch_size)
-    generator = m.time_series_generator()
-    history = m.fit()
+    m = Lstm(train_resids, test_resids, look_back,  output_nodes, nb_epoch, batch_size)
+    m.fit()
     lstm_prediction = m.predict()
-    info = list(params) + [m.mse(), m.rmse(), m.mae(), m.mape()]
-    
-    lstm_prediction = (lstm_prediction + 2) * (maximum - minimum) / 4 + minimum
+    print(type(m.mae()), "-----type (hybridKeywords.py)")
+    print(m.mae())
+    info = list(params) + [m.mse(), m.mae(),m.rmse()]
 
     return info, lstm_prediction
 
-def runLstm(train_resids, test_resids):
+
+def gridSearch(residuals):
+
+    m = 52 if (dd.time_series(keyword).index[1].month - dd.time_series(keyword).index[0].month) == 0 else 12
+
+    train_resids = residuals[:len(residuals)-int(m/2)]
+    test_resids = residuals[len(residuals)-int(m/2):]
 
     param_combinations = list(itertools.product(*params_lstm))
     print("number of combinations for grid search:", len(param_combinations))
-    
-    pool = mp.Pool(mp.cpu_count())
+
+    pool = mp.Pool(2)
     performance = pool.starmap(lstm, [(combination, train_resids, test_resids, param_combinations.index(combination)+1) for combination in param_combinations])
     performance = [p[0] for p in performance]
     pool.close()
 
-    performance_df = pd.DataFrame(performance, columns = ['look back', 'output nodes', 'epochs', 'batch size', 'MSE', 'MAE', 'RMSE', 'MAPE'])
+    performance_df = pd.DataFrame(performance, columns = ['look back', 'output nodes', 'epochs', 'batch size', 'MSE', 'MAE', 'RMSE'])
     print(performance_df)
 
     optimal_params = performance_df.iloc[performance_df.RMSE.argmin(), :4]
@@ -128,45 +107,45 @@ def runLstm(train_resids, test_resids):
 
     return np.array(optimal_params).astype(int)
 
-def runSarima(keyword):
-
-    m = 52 #if (dd.time_series.index[1].month - dd.time_series.index[0].month) == 0 else 12
-
-    sarima = dd.fit(keyword)
-    residuals = sarima.resid().reshape(-1,1)
-
-    train_resids = residuals[:len(residuals)-m]
-    test_resids = residuals[len(residuals)-m:] #Will be used for the performance measures
-
-    return sarima, train_resids, test_resids
 
 def main():
-
+    
     train_data = dd.time_series(keyword, True)
     test_data = dd.time_series(keyword, False)
-
     for p in params_lstm[1]: 
         if (len(test_data) % p != 0): sys.exit("The length of the test data is not a multiple of the output nodes")
 
-    sarima, train_resids, test_resids = runSarima(keyword) #4 jaar
-    optimal_params = runLstm(train_resids, test_resids) #[52, 1, 400, 156]
+    
+    sarima = dd.fit(keyword)
+    residuals = sarima.resid().reshape(-1,1)
+
+    optimal_params = gridSearch(residuals)
     sarima_forecast, lstm_forecast = getForecasts(sarima, optimal_params, test_data)
 
-    ################ OUTPUT AND PLOTTING:
     plot_hybrid(train_data, test_data, sarima_forecast, lstm_forecast)
 
-if __name__ == "__main__":
-    start_year = 2016
-    end_year = 2021
-    country = 'DE'
-    keyword = 'cluster6'
-    params_lstm = [[52, 65, 78, 91], [1], [400], [104, 156, 208]]
-    
+def cluster_df():
+
     cluster_mean = pd.read_csv('/Users/safouane/Desktop/cluster_means.csv')[keyword]
     df = ApiExtract.extract(range(start_year, end_year), country)[:len(cluster_mean.index)]
     df['interest'] = cluster_mean
     df['keyword'] = keyword
     df['category'] = 'cluster'
-    
-    dd = Arima(df, .8)
+
+    return df
+
+if __name__ == "__main__":
+
+    os.environ['TF_CPP_MIN_LOG_LEVEL'] = '1' 
+
+    start_year = 2016
+    end_year = 2021
+    country = 'ES'
+    keyword = 'cluster7'
+    params_lstm = [[52], [1], [100], [104]]#[[52], [1], [600, 800, 1000], [104, 208]]
+
+    df = cluster_df() if 'cluster' in keyword else ApiExtract.extract(range(start_year, end_year), country)
+    dd = Arima(df, .9)
+#    dd.get_covid_policy()
+
     main()
